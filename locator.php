2de9e00238a451a4310c4c0d80a6ebb68f3be2d6
Francesco Piero Paolicelli <piersoft@me.com>
2015-10-18 08:56:53 +0200

    prima versione

---
+++ locator.php
@@ -0,0 +1,276 @@
+<?php
+$lat=$_GET["lat"];
+$lon=$_GET["lon"];
+$r=$_GET["r"];
+
+function geoJson ($locales)
+    {
+        $original_data = json_decode($locales, true);
+        $features = array();
+
+        foreach($original_data as $key => $value) {
+            $features[] = array(
+                    'type' => 'Feature',
+                    'geometry' => array('type' => 'Point', 'coordinates' => array((float)$value['PosizioneFermata']['Longitudine'],(float)$value['PosizioneFermata']['Latitudine'])),
+                    'properties' => array('name' => $value['DescrizioneFermata'], 'id' => $value['IdFermata']),
+                    );
+            };
+
+        $allfeatures = array('type' => 'FeatureCollection', 'features' => $features);
+        return json_encode($allfeatures, JSON_PRETTY_PRINT);
+
+    }
+$url = 'http://bari.opendata.planetek.it/OrariBus/v2.1/OpenDataService.svc/REST/rete/FermateVicine/'.$lat.'/'.$lon.'/'.$r;
+$file = "mappa.json";
+
+$src = fopen($url, 'r');
+$dest = fopen($file, 'w');
+stream_copy_to_stream($src, $dest);
+
+$file1 = "mappaf.json";
+$original_json_string = file_get_contents('mappa.json', true);
+if($original_json_string=="[]")
+{
+  echo "<script type='text/javascript'>alert('Non ci sono fermate vicino alla tua posizione');</script>";
+
+}
+$dest1 = fopen($file1, 'w');
+
+$geostring=geoJson($original_json_string);
+
+fputs($dest1, $geostring);
+
+
+//echo stream_copy_to_stream($src, $dest) . "";
+//sleep(1);
+//header("Location:http://www.apposta.biz/prove/mappacqualta.html");
+
+?>
+
+<!DOCTYPE html>
+<html lang="it">
+  <head>
+  <title>Trasporti BARI Amtab</title>
+  <link rel="stylesheet" href="http://necolas.github.io/normalize.css/2.1.3/normalize.css" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
+  <link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet-0.7.5/leaflet.css" />
+        <link rel="stylesheet" href="MarkerCluster.css" />
+        <link rel="stylesheet" href="MarkerCluster.Default.css" />
+        <meta property="og:image" content="http://www.piersoft.it/baritrasportibot/bus_.png"/>
+  <script src="http://cdn.leafletjs.com/leaflet-0.7.5/leaflet.js"></script>
+   <script src="leaflet.markercluster.js"></script>
+<script type="text/javascript">
+
+function microAjax(B,A){this.bindFunction=function(E,D){return function(){return E.apply(D,[D])}};this.stateChange=function(D){if(this.request.readyState==4 ){this.callbackFunction(this.request.responseText)}};this.getRequest=function(){if(window.ActiveXObject){return new ActiveXObject("Microsoft.XMLHTTP")}else { if(window.XMLHttpRequest){return new XMLHttpRequest()}}return false};this.postBody=(arguments[2]||"");this.callbackFunction=A;this.url=B;this.request=this.getRequest();if(this.request){var C=this.request;C.onreadystatechange=this.bindFunction(this.stateChange,this);if(this.postBody!==""){C.open("POST",B,true);C.setRequestHeader("X-Requested-With","XMLHttpRequest");C.setRequestHeader("Content-Type","application/x-www-form-urlencoded; charset=UTF-8");C.setRequestHeader("Connection","close")}else{C.open("GET",B,true)}C.send(this.postBody)}};
+
+</script>
+  <style>
+  #mapdiv{
+        position:fixed;
+        top:0;
+        right:0;
+        left:0;
+        bottom:0;
+}
+#infodiv{
+background-color: rgba(255, 255, 255, 0.95);
+
+font-family: Helvetica, Arial, Sans-Serif;
+padding: 2px;
+
+
+font-size: 10px;
+bottom: 13px;
+left:0px;
+
+
+max-height: 50px;
+
+position: fixed;
+
+overflow-y: auto;
+overflow-x: hidden;
+}
+#loader {
+    position:absolute; top:0; bottom:0; width:100%;
+    background:rgba(255, 255, 255, 1);
+    transition:background 1s ease-out;
+    -webkit-transition:background 1s ease-out;
+}
+#loader.done {
+    background:rgba(255, 255, 255, 0);
+}
+#loader.hide {
+    display:none;
+}
+#loader .message {
+    position:absolute;
+    left:50%;
+    top:50%;
+}
+</style>
+  </head>
+
+<body>
+
+  <div data-tap-disabled="true">
+
+  <div id="mapdiv"></div>
+<div id="infodiv" style="leaflet-popup-content-wrapper">
+  <p><b>Fermate Bus BARI nelle vicinanze<br></b>
+  Mappa con fermate, linee e orarie dei Bus AMTAB di Bari by @piersoft. Fonte dati Lic. CC0 <a href="http://bari.opendata.planetek.it/OrariBus/v2.1/">SEMINA</a></p>
+</div>
+<div id='loader'><span class='message'>loading</span></div>
+</div>
+  <script type="text/javascript">
+		var lat=41.1181,
+        lon=16.8695,
+        zoom=14;
+        var osm = new L.TileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {maxZoom: 20, attribution: 'Map Data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'});
+		var mapquest = new L.TileLayer('http://otile{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png', {subdomains: '1234', maxZoom: 18, attribution: 'Map Data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'});
+
+        var map = new L.Map('mapdiv', {
+                    editInOSMControl: true,
+            editInOSMControlOptions: {
+                position: "topright"
+            },
+            center: new L.LatLng(lat, lon),
+            zoom: zoom,
+            layers: [osm]
+        });
+
+        var baseMaps = {
+    "Mapnik": osm,
+    "Mapquest Open": mapquest
+        };
+        L.control.layers(baseMaps).addTo(map);
+
+       var ico=L.icon({iconUrl:'icobusstop.png', iconSize:[40,60],iconAnchor:[20,0]});
+       var markers = L.markerClusterGroup({spiderfyOnMaxZoom: false, showCoverageOnHover: true,zoomToBoundsOnClick: true});
+
+        function loadLayer(url)
+        {
+                var myLayer = L.geoJson(url,{
+                        onEachFeature:function onEachFeature(feature, layer) {
+                                if (feature.properties && feature.properties.id) {
+                                }
+
+                        },
+                        pointToLayer: function (feature, latlng) {
+                        var marker = new L.Marker(latlng, { icon: ico });
+
+                        markers[feature.properties.id] = marker;
+                        marker.bindPopup('<img src="http://www.piersoft.it/baritrasportibot/ajax-loader.gif">',{maxWidth:50, autoPan:true});
+
+                      //  marker.on('click',showMarker());
+                        return marker;
+                        }
+                });
+                //.addTo(map);
+
+                markers.addLayer(myLayer);
+                map.addLayer(markers);
+                markers.on('click',showMarker);
+        }
+
+microAjax('mappaf.json',function (res) {
+var feat=JSON.parse(res);
+loadLayer(feat);
+  finishedLoading();
+} );
+function convertTimestamp(timestamp) {
+  var d = new Date(timestamp * 1000),	// Convert the passed timestamp to milliseconds
+		yyyy = d.getFullYear(),
+		mm = ('0' + (d.getMonth() + 1)).slice(-2),	// Months are zero based. Add leading 0.
+		dd = ('0' + d.getDate()).slice(-2),			// Add leading 0.
+		hh = d.getHours(),
+		h = hh,
+		min = ('0' + d.getMinutes()).slice(-2),		// Add leading 0.
+		ampm = 'AM',
+		time;
+
+	if (hh > 12) {
+	//	h = hh - 12;
+		ampm = 'PM';
+	} else if (hh === 12) {
+	//	h = 12;
+		ampm = 'PM';
+	} else if (hh == 0) {
+		h = 12;
+	}
+
+	// ie: 2013-02-18, 8:35 AM
+	time = h + ':' + min;
+
+	return time;
+}
+
+ function showMarker(marker) {
+
+   var jsonref=marker.layer.feature;
+
+   microAjax('http://bari.opendata.planetek.it/OrariBus/v2.1/OpenDataService.svc/REST/OrariPalina/'+jsonref.properties.id+'/', function (res) {
+
+   var feat=JSON.parse(res);
+   var index;
+   console.log(feat.length);
+//  alert (feat.length);
+var text;
+var i = 0;
+
+
+if(feat['PrevisioniLinee'].length != "undefined")
+{
+  if(feat['PrevisioniLinee'].length == "0")
+  {
+  text ="Non ci sono linee in arrivo nelle prossime ore";
+  marker.layer.closePopup();
+  marker.layer.bindPopup(text);
+  marker.layer.openPopup();
+  console.log("Feat lenght: "+feat['PrevisioniLinee'].length);
+}else{
+
+  text ="Prossimo arrivo: <b>";
+console.log("Feat lenght: "+feat['PrevisioniLinee'].length);
+for (i=0;i<feat['PrevisioniLinee'].length;i++){
+    //   // when the tiles load, remove the screen
+    var last=feat['PrevisioniLinee'][i];
+  //  var text ="Linee servite: "+last['IdLinea']+"<br>";
+    text +="<br />"+last['IdLinea']+" "+last['DirezioneLinea'];
+    var orario =last['OrarioArrivo'];
+    orario= orario.replace("/Date(","");
+    orario=orario.replace("000+0200)/","");
+    var date=convertTimestamp(orario);
+    //var date = new Date(orario*1000);
+    //var iso = date.toISOString().match(/(\d{2}:\d{2}:\d{2})/);
+    text+=" Arrivo: "+date;
+    marker.layer.closePopup();
+    marker.layer.bindPopup(text);
+    marker.layer.openPopup();
+  }
+}
+}
+
+}
+  );
+
+}
+function startLoading() {
+    loader.className = '';
+}
+
+function finishedLoading() {
+    // first, toggle the class 'done', which makes the loading screen
+    // fade out
+    loader.className = 'done';
+    setTimeout(function() {
+        // then, after a half-second, add the class 'hide', which hides
+        // it completely and ensures that the user can interact with the
+        // map again.
+        loader.className = 'hide';
+    }, 500);
+}
+</script>
+
+</body>
+</html>
---
+++ main.php
@@ -0,0 +1,374 @@
+<?php
+/**
+* Telegram Bot example for Public Transport of Bari (Italy)
+* @author @Piersoft
+Funzionamento
+- invio location
+- invio fermata più vicina come risposta
+
+
+*/
+
+include("Telegram.php");
+
+class mainloop{
+const MAX_LENGTH = 4096;
+function start($telegram,$update)
+{
+
+	date_default_timezone_set('Europe/Rome');
+	$today = date("Y-m-d H:i:s");
+	$db = new PDO(DB_NAME);
+
+	$text = $update["message"] ["text"];
+	$chat_id = $update["message"] ["chat"]["id"];
+	$user_id=$update["message"]["from"]["id"];
+	$location=$update["message"]["location"];
+	$reply_to_msg=$update["message"]["reply_to_message"];
+
+	$this->shell($telegram, $db,$text,$chat_id,$user_id,$location,$reply_to_msg);
+	$db = NULL;
+
+}
+
+//gestisce l'interfaccia utente
+ function shell($telegram,$db,$text,$chat_id,$user_id,$location,$reply_to_msg)
+{
+	date_default_timezone_set('Europe/Rome');
+	$today = date("Y-m-d H:i:s");
+
+	if ($text == "/start") {
+		$reply = "Benvenuto. Invia la tua posizione cliccando sulla graffetta (📎) e ti indicherò le fermate AMTAB di Bari più vicine nel raggio di 200 metri e relative linee ed orari";
+		$reply .= "\nI dati AMBTAB, in licenza opendata CC0 Universal, sono prelevabili realtime su http://bari.opendata.planetek.it/OrariBus/v2.1/";
+		$reply .= "\nLe risposte avverranno ogni 60 secondi";
+		$reply .= "\nProgetto sviluppato da @Piersoft. Si declina ogni responsabilità sulla veridicità dei dati.";
+
+		$content = array('chat_id' => $chat_id, 'text' => $reply);
+		$telegram->sendMessage($content);
+
+		$forcehide=$telegram->buildKeyBoardHide(true);
+		$content = array('chat_id' => $chat_id, 'text' => "", 'reply_markup' =>$forcehide, 'reply_to_message_id' =>$bot_request_message_id);
+		$bot_request_message=$telegram->sendMessage($content);
+		$log=$today. ";new chat started;" .$chat_id. "\n";
+	}elseif ($text == "/linee" || $text =="linee") {
+	//$response=$telegram->getData();
+		$temp_c1="";
+	//	$bot_request_message_id=$response["message"]["message_id"];
+		$json_string = file_get_contents("http://bari.opendata.planetek.it/OrariBus/v2.1/OpenDataService.svc/REST/rete/Linee");
+		$parsed_json = json_decode($json_string);
+		$count =0;
+		foreach($parsed_json as $data=>$csv1){
+			 $count = $count+1;
+		}
+	//	echo 	"Linee: ".$count." ".$parsed_json[0]->{'IdLinea'};
+
+		for ($i=0;$i<$count;$i++){
+
+		$temp_c1 =$parsed_json[$i]->{'IdLinea'}." - ".$parsed_json[$i]->{'DescrizioneLinea'};
+		//$content = array('chat_id' => $chat_id, 'text' => $chunk, 'reply_to_message_id' =>$bot_request_message_id,'disable_web_page_preview'=>true);
+		//$telegram->sendMessage($content);
+
+		$chunks = str_split($temp_c1, self::MAX_LENGTH);
+	  foreach($chunks as $chunk) {
+	 	// $forcehide=$telegram->buildForceReply(true);
+	 		 //chiedo cosa sta accadendo nel luogo
+	 		 $content = array('chat_id' => $chat_id, 'text' => $chunk,'disable_web_page_preview'=>true);
+	 		 $telegram->sendMessage($content);
+
+	  }
+	}
+
+
+}elseif ($text == "01") {
+//	$response=$telegram->getData();
+	$temp_c1="";
+	$h = "2";// Hour for time zone goes here e.g. +7 or -4, just remove the + or -
+	$hm = $h * 60;
+	$ms = $hm * 60;
+//	$bot_request_message_id=$response["message"]["message_id"];
+	$json_string = file_get_contents("http://bari.opendata.planetek.it/OrariBus/v2.1/OpenDataService.svc/REST/ServizioGiornaliero/".$text."/");
+	$parsed_json = json_decode($json_string);
+	$count =0;
+
+	foreach($parsed_json as $data=>$csv1){
+		 $count = $count+1;
+	}
+
+		$time =$parsed_json[0]->{'Orario'}; //registro nel DB anche il tempo unix
+		$time =str_replace("/Date(","",$time);
+		$time =str_replace("000+0200)/","",$time);
+
+		$time =str_replace(" ","",$time);
+		$time =str_replace("\n","",$time);
+		$timef=floatval($time);
+		$timeff = time();
+		$timec =gmdate('H:i:s', $timef+$ms);
+
+		$temp_c1 .="\narrivo: ".$timec;
+
+
+	$fermataid =$parsed_json[0]->{'IdFermata'};
+	echo $fermataid;
+	$json_stringf = file_get_contents("http://bari.opendata.planetek.it/OrariBus/v2.1/OpenDataService.svc/REST/rete/Fermate/".$fermataid);
+	$parsed_jsonf = json_decode($json_stringf);
+
+  $fermata=$parsed_jsonf[0]->{'DescrizioneFermata'};
+
+	echo "\nLinea: ".$text."\nFermata".$fermata."\norario: ".$timec;
+
+	for ($i=0;$i<$count;$i++){
+
+
+	$time =$parsed_json[$i]->{'Orario'}; //registro nel DB anche il tempo unix
+	$time =str_replace("/Date(","",$time);
+	$time =str_replace("000+0200)/","",$time);
+
+	$time =str_replace(" ","",$time);
+	$time =str_replace("\n","",$time);
+	$timef=floatval($time);
+	$timeff = time();
+	$timec =gmdate('H:i:s', $timef+$ms);
+
+	$temp_c1 .="\narrivo: ".$timec;
+
+	//echo $temp_c1;
+}
+}
+		//gestione segnalazioni georiferite
+		elseif($location!=null)
+		{
+
+			$this->location_manager($db,$telegram,$user_id,$chat_id,$location);
+			exit;
+
+		}else{
+
+$forcehide=$telegram->buildKeyBoardHide(true);
+$content = array('chat_id' => $chat_id, 'text' => "Comando errato.\nInvia la tua posizione cliccando sulla graffetta (📎) in basso e, se vuoi, puoi cliccare due volte sulla mappa e spostare il Pin Rosso in un luogo di cui vuoi conoscere le fermate più vicine. Risposta entro 60 secondi.", 'reply_markup' =>$forcehide);
+$telegram->sendMessage($content);
+	//		$this->create_keyboard($telegram,$chat_id);
+			exit;
+
+		}
+
+		file_put_contents(LOG_FILE, $log, FILE_APPEND | LOCK_EX);
+
+}
+
+
+// Crea la tastiera
+ function create_keyboard($telegram, $chat_id)
+	{
+		$forcehide=$telegram->buildKeyBoardHide(true);
+		$content = array('chat_id' => $chat_id, 'text' => "Invia la tua posizione cliccando sulla graffetta (📎) in basso e, se vuoi, puoi cliccare due volte sulla mappa e spostare il Pin Rosso in un luogo di cui vuoi conoscere le fermate più vicine. Risposta entro 60 secondi.", 'reply_markup' =>$forcehide);
+		$telegram->sendMessage($content);
+
+	}
+
+
+
+function location_manager($db,$telegram,$user_id,$chat_id,$location)
+	{
+
+			$lng=$location["longitude"];
+			$lat=$location["latitude"];
+      $r=100;
+			//rispondo
+			$response=$telegram->getData();
+
+			$bot_request_message_id=$response["message"]["message_id"];
+			$time=$response["message"]["date"]; //registro nel DB anche il tempo unix
+
+			$h = "2";// Hour for time zone goes here e.g. +7 or -4, just remove the + or -
+			$hm = $h * 60;
+			$ms = $hm * 60;
+			$timec=gmdate("Y-m-d\TH:i:s\Z", $time+($ms));
+			$timec=str_replace("T"," ",$timec);
+			$timec=str_replace("Z"," ",$timec);
+
+			$json_string = file_get_contents("http://bari.opendata.planetek.it/OrariBus/v2.1/OpenDataService.svc/REST/rete/FermateVicine/".$lat."/".$lng."/200");
+			$parsed_json = json_decode($json_string);
+			$count =0;
+			$countl = [];
+			foreach($parsed_json as $data=>$csv1){
+		     $count = $count+1;
+			}
+			if ($count ==0){
+				$content = array('chat_id' => $chat_id, 'text' => 'Nessuna fermata trovata', 'reply_to_message_id' =>$bot_request_message_id);
+				$telegram->sendMessage($content);
+					$this->create_keyboard($telegram,$chat_id);
+				exit;
+			}
+	//    echo "Fermate più vicine rispetto a ".$lat."/".$lng." in raggio di ".$r." metri con relative linee urbane ed orari arrivi\n";
+
+		$IdFermata="";
+		$temp_c1="";
+		$temp_l1="";
+		$option=array();
+
+	for ($i=0;$i<$count;$i++){
+		foreach($parsed_json[$i]->{'ListaLinee'} as $data=>$csv1){
+			 $countl[$i] = $countl[$i]+1;
+
+			}
+
+			echo "countl: ".$countl[$i];
+		//echo $countl;
+			$temp_c1 .="Fermata: ".$parsed_json[$i]->{'DescrizioneFermata'};
+			//."\nId Fermata: ".$parsed_json[$i]->{'IdFermata'};
+			$longUrl="http://www.openstreetmap.org/?mlat=".$parsed_json[$i]->{'PosizioneFermata'}->{'Latitudine'}."&mlon=".$parsed_json[$i]->{'PosizioneFermata'}->{'Longitudine'}."#map=19/".$parsed_json[$i]->{'PosizioneFermata'}->{'Latitudine'}."/".$parsed_json[$i]->{'PosizioneFermata'}->{'Longitudine'}."/".$_POST['qrname'];
+
+		//	$longUrl= "http://umap.openstreetmap.fr/it/map/segnalazioni-con-opendataleccebot-x-interni_54105#19/".$row[0]['lat']."/".$row[0]['lng']."/".$_POST['qrname'];
+			$apiKey = API;
+
+			$postData = array('longUrl' => $longUrl, 'key' => $apiKey);
+			$jsonData = json_encode($postData);
+
+			$curlObj = curl_init();
+
+			curl_setopt($curlObj, CURLOPT_URL, 'https://www.googleapis.com/urlshortener/v1/url?key='.$apiKey);
+			curl_setopt($curlObj, CURLOPT_RETURNTRANSFER, 1);
+			curl_setopt($curlObj, CURLOPT_SSL_VERIFYPEER, 0);
+			curl_setopt($curlObj, CURLOPT_HEADER, 0);
+			curl_setopt($curlObj, CURLOPT_HTTPHEADER, array('Content-type:application/json'));
+			curl_setopt($curlObj, CURLOPT_POST, 1);
+			curl_setopt($curlObj, CURLOPT_POSTFIELDS, $jsonData);
+
+			$response = curl_exec($curlObj);
+
+			// Change the response json string to object
+			$json = json_decode($response);
+
+			curl_close($curlObj);
+		//  $reply="Puoi visualizzarlo su :\n".$json->id;
+			$shortLink = get_object_vars($json);
+		//return $json->id;
+
+			$temp_c1 .="\nVisualizzala su :\n".$shortLink['id'];
+
+		if ($countl[$i]!=0){
+			$temp_c1 .="\nLinee servite :";
+		}	else $temp_c1 .="\n";
+
+
+			for ($l=0;$l<$countl[$i];$l++)
+				{
+
+
+			$temp_l1 =$parsed_json[$i]->{'ListaLinee'}[$l]->{'IdLinea'}." ".$parsed_json[$i]->{'ListaLinee'}[$l]->{'Direzione'};
+			array_push($option, $temp_l1);
+
+			$temp_c1 .=" ".$parsed_json[$i]->{'ListaLinee'}[$l]->{'IdLinea'}.".".$parsed_json[$i]->{'ListaLinee'}[$l]->{'Direzione'};
+				 }
+			$temp_c1 .="";
+
+
+			// inzio sotto routine per orari per linee afferenti alla fermata:
+
+			$IdFermata=$parsed_json[$i]->{'IdFermata'};
+	    //	echo $IdFermata;
+			$json_string1 = file_get_contents("http://bari.opendata.planetek.it/OrariBus/v2.1/OpenDataService.svc/REST/OrariPalina/".$IdFermata."/");
+			$parsed_json1 = json_decode($json_string1);
+		//  var_dump($parsed_json1);
+		//  var_dump($parsed_json1->{'PrevisioniLinee'}[0]);
+			$countf = 0 ;
+			foreach($parsed_json1->{'PrevisioniLinee'} as $data123=>$csv113){
+				 $countf = $countf+1;
+			}
+
+
+	//    echo $countf;
+			$h = "2";// Hour for time zone goes here e.g. +7 or -4, just remove the + or -
+			$hm = $h * 60;
+			$ms = $hm * 60;
+			date_default_timezone_set('UTC');
+			for ($f=0;$f<$countf;$f++){
+			//	$corsa = get_object_vars($parsed_json1);
+			//return $json->id;
+
+		//		$check =$shortLink['IdLinea'];
+		//		if(!$parsed_json1->{'IdCorsa'}[$f]) alert();
+
+			$check=$parsed_json1->{'PrevisioniLinee'}[$f]->{'IdLinea'};
+			if (empty($check)){
+				$content = array('chat_id' => $chat_id, 'text' => 'Non ci sono linee', 'reply_to_message_id' =>$bot_request_message_id,'disable_web_page_preview'=>true);
+				$telegram->sendMessage($content);
+
+       }
+				$time =$parsed_json1->{'PrevisioniLinee'}[$f]->{'OrarioArrivo'}; //registro nel DB anche il tempo unix
+		//    echo "\ntimestamp:".$time."senza pulizia dati";
+				$time =str_replace("/Date(","",$time);
+				$time =str_replace("000+0200)/","",$time);
+		//    $time =str_replace("T"," ",$time);
+		//    $time =str_replace("Z"," ",$time);
+				$time =str_replace(" ","",$time);
+				$time =str_replace("\n","",$time);
+				$timef=floatval($time);
+				$timeff = time();
+				$timec =gmdate('H:i:s', $timef+$ms);
+
+				$temp_c1 .="\nLinea: ".$parsed_json1->{'PrevisioniLinee'}[$f]->{'IdLinea'}." ".$parsed_json1->{'PrevisioniLinee'}[$f]->{'DirezioneLinea'}." arrivo: ".$timec."";
+			 }
+				$temp_c1 .="\n\n";
+
+
+	}
+
+$longUrl="http://www.piersoft.it/baritrasportibot/locator.php?lat=".$lat."&lon=".$lng."&r=200";
+$apiKey = API;
+
+$postData = array('longUrl' => $longUrl, 'key' => $apiKey);
+$jsonData = json_encode($postData);
+
+$curlObj = curl_init();
+
+curl_setopt($curlObj, CURLOPT_URL, 'https://www.googleapis.com/urlshortener/v1/url?key='.$apiKey);
+curl_setopt($curlObj, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($curlObj, CURLOPT_SSL_VERIFYPEER, 0);
+curl_setopt($curlObj, CURLOPT_HEADER, 0);
+curl_setopt($curlObj, CURLOPT_HTTPHEADER, array('Content-type:application/json'));
+curl_setopt($curlObj, CURLOPT_POST, 1);
+curl_setopt($curlObj, CURLOPT_POSTFIELDS, $jsonData);
+
+$response = curl_exec($curlObj);
+
+// Change the response json string to object
+$json = json_decode($response);
+
+curl_close($curlObj);
+//  $reply="Puoi visualizzarlo su :\n".$json->id;
+$shortLink = get_object_vars($json);
+//return $json->id;
+
+$temp_c1 .="\nVisualizza tutte le fermate a te vicine su :\n".$shortLink['id'];
+
+
+
+ $chunks = str_split($temp_c1, self::MAX_LENGTH);
+ foreach($chunks as $chunk) {
+	// $forcehide=$telegram->buildForceReply(true);
+		 //chiedo cosa sta accadendo nel luogo
+		 $content = array('chat_id' => $chat_id, 'text' => $chunk, 'reply_to_message_id' =>$bot_request_message_id,'disable_web_page_preview'=>true);
+		 $telegram->sendMessage($content);
+
+ }
+ //$telegram->sendMessage($content);
+	echo $temp_l1;
+
+//if ($temp_l1 ==="") {
+//	$content = array('chat_id' => $chat_id, 'text' => "Nessuna fermata nei paraggi", 'reply_to_message_id' =>$bot_request_message_id);
+//		$telegram->sendMessage($content);
+
+//}
+	$today = date("Y-m-d H:i:s");
+
+	$log=$today. ";fermatebari sent;" .$chat_id. "\n";
+	$this->create_keyboard($telegram,$chat_id);
+	exit;
+
+	}
+
+
+}
+
+?>
